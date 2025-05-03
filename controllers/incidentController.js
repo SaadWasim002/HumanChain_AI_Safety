e8dbbd25549567f185f29ba67311935807809b70
SaadWasim02 <saadwasim444@gmail.com>
2025-05-04 01:38:42 +0530

    Added Api response and Api error

--- controllers/incidentController.js
+++ controllers/incidentController.js
@@ -1,13 +1,16 @@
 import Incident from "../models/Incident.js";
+import { ApiError } from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
 
 // GET /incidents
 export const getAllIncidents = async (req, res) => {
     try {
         const incidents = await Incident.find();
-        res.json(incidents);
+        res.status(200).json(
+            new ApiResponse(200 , incidents , "All incidents retrieved")
+        );
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Server Error" });
+        throw new ApiError(500 , `Server Error : ${err}`);
     }
 };
 
@@ -16,12 +19,15 @@ export const getIncidentById = async (req, res) => {
     try {
         const incident = await Incident.findById(req.params.id);
         if(!incident) {
-            return res.status(404).json({ message: "Incident not found" });
+            return res.status(404).json(
+                new ApiResponse(404 , {} , "Incident not found" , false)
+            )
         }
-        res.status(200).json(incident);
+        res.status(200).json(
+            new ApiResponse(200 , incident , "Incident retrieved successfully")
+        );
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Server Error" });
+        throw new ApiError(500 , `Server Error : ${err}`);
     }
 };
 
@@ -31,20 +37,25 @@ export const createIncident = async (req, res) => {
     const allowed = ["Low", "Medium", "High"];
 
     if(!title || !description || !severity) {
-        return res.status(400).json({ message: "All fields are required" });
+        return res.status(400).json(
+            new ApiResponse(400 , {} , "All fields are required" , false)
+        )
     }
 
     if(!allowed.includes(severity)) {
-        return res.status(400).json({ message: "Severity must be Low, Medium, or High" });
+        return res.status(400).json(
+            new ApiResponse(400 , {} , "Severity must be Low, Medium, or High" , false)
+        )
     }
 
     try {
         const incident = new Incident({ title, description, severity });
         const saved = await incident.save();
-        res.status(201).json(saved);
+        res.status(201).json(
+            new ApiResponse(201 , saved , "Incident created successfully")
+        );
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Server Error" });
+        throw new ApiError(500 , `Server Error : ${err}`);
     }
 };
 
@@ -53,13 +64,16 @@ export const deleteIncident = async (req, res) => {
     try {
         const incident = await Incident.findById(req.params.id);
         if(!incident) {
-            return res.status(404).json({ message: "Incident not found" });
+            return res.status(404).json(
+                new ApiResponse(404 , {} , "Incident not found" , false)
+            )
         }
         await incident.deleteOne();
-        res.status(200).json({ message: "Incident deleted successfully" });
+        res.status(200).json(
+            new ApiResponse(200 , {} , "Incident deleted successfully")
+        )
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Server Error" });
+        throw new ApiError(500 , `Server Error : ${err}`);
     }
 };
 
